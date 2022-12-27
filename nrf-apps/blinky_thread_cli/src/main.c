230214123f8d259a65279c3362b55e97216fca84
LyubomirNachev <lubolubo@gmail.com>
2022-12-27 15:46:43 +0200

    Found that example works when configured with console parameter in device tree but with shell-uart doesn't

--- nrf-apps/blinky_thread_cli/src/main.c
+++ nrf-apps/blinky_thread_cli/src/main.c
@@ -1,41 +1,47 @@
-#include <zephyr/zephyr.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
+
 
 
 void main(void)
 {
-	while (1) {
-		k_msleep(SLEEP_TIME_MS);
+	int ret;
+	const struct device *dev;
+	uint32_t dtr = 0U;
+
+	ret = usb_enable(NULL);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return;
 	}
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (dev == NULL) {
+		LOG_ERR("Failed to find specific UART device");
+		return;
+	}
+
+	LOG_INF("Waiting for host to be ready to communicate");
+
+	/* Data Terminal Ready - check if host is ready to communicate */
+	while (!dtr) {
+		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		if (ret) {
+			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
+				ret);
+			continue;
+		}
+		k_msleep(100);
+	}
+
+	/* Data Carrier Detect Modem - mark connection as established */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
+	/* Data Set Ready - the NCP SoC is ready to communicate */
+	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+
+
 }
-// #include <zephyr/zephyr.h>
-// #include <zephyr/sys/printk.h>
-// #include <zephyr/usb/usb_device.h>
-// #include <zephyr/drivers/uart.h>
-
-// void main(void)
-// {
-// 	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-// 	uint32_t dtr = 0;
-
-// 	if (usb_enable(NULL)) {
-// 		return;
-// 	}
-
-// 	/* Poll if the DTR flag was set */
-// 	while (!dtr) {
-// 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-// 		/* Give CPU resources to low priority threads. */
-// 		k_sleep(K_MSEC(100));
-// 	}
-
-// 	while (1) {
-// 		printk("Hello World! %s\n", CONFIG_ARCH);
-// 		k_sleep(K_SECONDS(1));
-// 	}
-// }
