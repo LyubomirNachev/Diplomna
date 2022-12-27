57a349d4ac7bbd8b11b6c167b6d547efe5e7e6f7
LyubomirNachev <lubolubo@gmail.com>
2022-12-27 23:45:59 +0200

    Added bh1750 and console

---
+++ nrf-apps/console/src/main.c
@@ -0,0 +1,26 @@
+#include <zephyr/zephyr.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+
+void main(void)
+{
+	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+
+	while (1) {
+		printk("Hello World! %s\n", CONFIG_ARCH);
+		k_sleep(K_SECONDS(1));
+	}
+}
