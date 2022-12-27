295e6367db1da11ec2b2875f6ce802921d79b804
LyubomirNachev <lubolubo@gmail.com>
2022-12-27 16:17:09 +0200

    Made working usb console that can print

--- nrf-apps/blinky_thread_cli/src/main.c
+++ nrf-apps/blinky_thread_cli/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/usb/usb_device.h>
+#include <zephyr/sys/printk.h>
 
 LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
 
@@ -15,33 +16,35 @@ void main(void)
 
 	ret = usb_enable(NULL);
 	if (ret != 0) {
-		LOG_ERR("Failed to enable USB");
+		printk("Failed to enable USB");
 		return;
 	}
 
-	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell-uart));
 	if (dev == NULL) {
-		LOG_ERR("Failed to find specific UART device");
+		printk("Failed to find specific UART device");
 		return;
 	}
 
-	LOG_INF("Waiting for host to be ready to communicate");
+	printk("Waiting for host to be ready to communicate");
 
 	/* Data Terminal Ready - check if host is ready to communicate */
 	while (!dtr) {
 		ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		if (ret) {
-			LOG_ERR("Failed to get Data Terminal Ready line state: %d",
+			printk("Failed to get Data Terminal Ready line state: %d",
 				ret);
 			continue;
 		}
 		k_msleep(100);
 	}
-
+	while(1){
+		printk("It works!\n");
+		printk("It does!\n");
+		k_msleep(1000);
+	}
 	/* Data Carrier Detect Modem - mark connection as established */
 	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
 	/* Data Set Ready - the NCP SoC is ready to communicate */
 	(void)uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
-
-
 }
