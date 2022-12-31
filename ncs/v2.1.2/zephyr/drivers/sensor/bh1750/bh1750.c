ada83bdd0c1755886488adb813dfe2abd71e513c
LyubomirNachev <lubolubo04@gmail.com>
2022-12-31 03:33:43 +0200

    Edited the bh1750 files

--- ncs/v2.1.2/zephyr/drivers/sensor/bh1750/bh1750.c
+++ ncs/v2.1.2/zephyr/drivers/sensor/bh1750/bh1750.c
@@ -1,12 +1,4 @@
-/* sensor_isl29035.c - driver for ISL29035 light sensor */
-
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#define DT_DRV_COMPAT isil_isl29035
+#define DT_DRV_COMPAT rohm_bh1750
 
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
@@ -15,147 +7,61 @@
 #include <zephyr/sys/__assert.h>
 #include <zephyr/logging/log.h>
 
-#include "isl29035.h"
+#include "bh1750.h"
+LOG_MODULE_REGISTER(BH1750, CONFIG_SENSOR_LOG_LEVEL);
 
-LOG_MODULE_REGISTER(ISL29035, CONFIG_SENSOR_LOG_LEVEL);
+float measuringTimeFactor;
 
-static int isl29035_sample_fetch(const struct device *dev,
-				 enum sensor_channel chan)
-{
-	struct isl29035_driver_data *drv_data = dev->data;
-	const struct isl29035_config *config = dev->config;
-	uint8_t msb, lsb;
-
-	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
-
-	if (i2c_reg_read_byte_dt(&config->i2c,
-				 ISL29035_DATA_MSB_REG, &msb) < 0) {
-		return -EIO;
+void getLux(){
+	uint16_t raw_lux;
+	float lux;
+	raw_lux = readBH1750();
+	if((mode==CHM_2)||(mode==OTH_2)){
+		lux = (raw_lux/2.4)/measuringTimeFactor;
+	}else{
+		lux = (raw_lux/1.2)/measuringTimeFactor;
 	}
-
-	if (i2c_reg_read_byte_dt(&config->i2c, ISL29035_DATA_LSB_REG,
-				 &lsb) < 0) {
-		return -EIO;
-	}
-
-	drv_data->data_sample = (msb << 8) + lsb;
-
-	return 0;
+	printk("Light: %flx", lux);
 }
 
-static int isl29035_channel_get(const struct device *dev,
-				enum sensor_channel chan,
-				struct sensor_value *val)
-{
-	struct isl29035_driver_data *drv_data = dev->data;
-	uint64_t tmp;
-
-#if CONFIG_ISL29035_MODE_ALS
-	/* val = sample_val * lux_range / (2 ^ adc_data_bits) */
-	tmp = (uint64_t)drv_data->data_sample * ISL29035_LUX_RANGE;
-	val->val1 = tmp >> ISL29035_ADC_DATA_BITS;
-	tmp = (tmp & ISL29035_ADC_DATA_MASK) * 1000000U;
-	val->val2 = tmp >> ISL29035_ADC_DATA_BITS;
-#elif CONFIG_ISL29035_MODE_IR
-	ARG_UNUSED(tmp);
-	val->val1 = drv_data->data_sample;
-	val->val2 = 0;
-#endif
-
-	return 0;
+void powerDown(){
+	writeBH1750(BH1750_POWER_DOWN);
 }
 
-static const struct sensor_driver_api isl29035_api = {
-#if CONFIG_ISL29035_TRIGGER
-	.attr_set = &isl29035_attr_set,
-	.trigger_set = &isl29035_trigger_set,
-#endif
-	.sample_fetch = &isl29035_sample_fetch,
-	.channel_get = &isl29035_channel_get,
-};
-
-static int isl29035_init(const struct device *dev)
-{
-	struct isl29035_driver_data *drv_data = dev->data;
-	const struct isl29035_config *config = dev->config;
-
-	if (!device_is_ready(config->i2c.bus)) {
-		LOG_ERR("I2C bus device not ready");
-		return -ENODEV;
-	}
-
-	drv_data->data_sample = 0U;
-
-	/* clear blownout status bit */
-	if (i2c_reg_update_byte_dt(&config->i2c, ISL29035_ID_REG,
-				   ISL29035_BOUT_MASK, 0) < 0) {
-		LOG_DBG("Failed to clear blownout status bit.");
-		return -EIO;
-	}
-
-	/* set command registers to set default attributes */
-	if (i2c_reg_write_byte_dt(&config->i2c,
-				  ISL29035_COMMAND_I_REG, 0) < 0) {
-		LOG_DBG("Failed to clear COMMAND-I.");
-		return -EIO;
-	}
-
-	if (i2c_reg_write_byte_dt(&config->i2c,
-				  ISL29035_COMMAND_II_REG, 0) < 0) {
-		LOG_DBG("Failed to clear COMMAND-II.");
-		return -EIO;
-	}
-
-	/* set operation mode */
-	if (i2c_reg_update_byte_dt(&config->i2c,
-				   ISL29035_COMMAND_I_REG,
-				   ISL29035_OPMODE_MASK,
-				   ISL29035_ACTIVE_OPMODE_BITS) < 0) {
-		LOG_DBG("Failed to set opmode.");
-		return -EIO;
-	}
-
-	/* set lux range */
-	if (i2c_reg_update_byte_dt(&config->i2c,
-				   ISL29035_COMMAND_II_REG,
-				   ISL29035_LUX_RANGE_MASK,
-				   ISL29035_LUX_RANGE_BITS) < 0) {
-		LOG_DBG("Failed to set lux range.");
-		return -EIO;
-	}
-
-	/* set ADC resolution */
-	if (i2c_reg_update_byte_dt(&config->i2c,
-				   ISL29035_COMMAND_II_REG,
-				   ISL29035_ADC_RES_MASK,
-				   ISL29035_ADC_RES_BITS) < 0) {
-		LOG_DBG("Failed to set ADC resolution.");
-		return -EIO;
-	}
-
-#ifdef CONFIG_ISL29035_TRIGGER
-	if (config->int_gpio.port) {
-		if (isl29035_init_interrupt(dev) < 0) {
-			LOG_DBG("Failed to initialize interrupt.");
-			return -EIO;
-		}
-	}
-#endif
-
-	return 0;
+void powerOn(){
+	writeBH1750(BH1750_POWER_ON);
+	setMode();
 }
 
-#define ISL29035_DEFINE(inst)									\
-	static struct isl29035_driver_data isl29035_data_##inst;				\
-												\
-	static const struct isl29035_config isl29035_config_##inst = {				\
-		.i2c = I2C_DT_SPEC_INST_GET(inst),						\
-		IF_ENABLED(CONFIG_ISL29035_TRIGGER,						\
-			   (.int_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, int_gpios, { 0 }),))	\
-	};											\
-												\
-	DEVICE_DT_INST_DEFINE(inst, &isl29035_init, NULL,					\
-			      &isl29035_data_##inst, &isl29035_config_##inst, POST_KERNEL,	\
-			      CONFIG_SENSOR_INIT_PRIORITY, &isl29035_api);			\
+void dataRegReset(){
+  writeBH1750(BH1750_DATA_REG_RESET);
+}
 
-DT_INST_FOREACH_STATUS_OKAY(ISL29035_DEFINE)
+void setMode(){
+  writeBH1750(mode);
+}
+
+void setMeasuringTime(){
+  uint8_t mt = round(measuringTimeFactor*69);
+  uint8_t highByteMT = ((mt>>5) | 0b01000000);
+  uint8_t lowByteMT = (mt & 0b01111111);
+  lowByteMT |= 0b01100000;
+  writeBH1750(highByteMT);
+  writeBH1750(lowByteMT);
+}
+
+uint16_t readBH1750(){
+  uint8_t MSbyte, LSbyte;
+  //Wire.requestFrom(BH_1750, 2);
+  //if(Wire.available()){
+    //MSbyte=Wire.read();
+    //LSbyte=Wire.read(); 
+  //}
+  return ((MSbyte<<8) + LSbyte);
+}
+
+void writeBH1750(uint8_t val){
+  //Wire.beginTransmission(BH_1750);
+  //Wire.write(val);
+  //Wire.endTransmission();
+}
