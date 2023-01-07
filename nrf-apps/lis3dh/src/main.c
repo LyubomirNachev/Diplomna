5d9681c101d6711512ceb04b9d6cb65a4daef8f6
LyubomirNachev <lubolubo04@gmail.com>
2023-01-07 18:52:16 +0200

    Added library for lis3dh, made minor changes

--- nrf-apps/lis3dh/src/main.c
+++ nrf-apps/lis3dh/src/main.c
@@ -6,15 +6,56 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/drivers/uart.h>
 
+#include <zephyr/sys/util.h>
+#include <zephyr/drivers/i2c.h>
+
+
+
 static void fetch_and_display(const struct device *sensor)
 {
 	static unsigned int count;
 	struct sensor_value accel[3];
-	struct sensor_value temperature;
 	const char *overrun = "";
 	int rc = sensor_sample_fetch(sensor);
+
+	
+	++count;
+	if (rc == -EBADMSG) {
+		/* Sample overrun.  Ignore in polled mode. */
+		if (IS_ENABLED(CONFIG_LIS3DH_TRIGGER)) {
+			overrun = "[OVERRUN] ";
+		}
+		rc = 0;
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor,
+					SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc < 0) {
+		printk("ERROR: Update failed: %d\n", rc);
+	} else {
+		printk("#%u @ %u ms: %sx %f , y %f , z %f",
+		       count, k_uptime_get_32(), overrun,
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]));
+	}
+}
+
+#ifdef CONFIG_LIS3DH_TRIGGER
+static void trigger_handler(const struct device *dev,
+			    const struct sensor_trigger *trig)
+{
+	fetch_and_display(dev);
+}
+#endif
+
+void main(void)
+{
 	const struct device *dev1 = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 	uint32_t dtr = 0;
+	const struct device *sensor = DEVICE_DT_GET_ANY(st_lis3dh);
 
 	if (usb_enable(NULL)) {
 		return;
@@ -26,66 +67,17 @@ static void fetch_and_display(const struct device *sensor)
 		/* Give CPU resources to low priority threads. */
 		k_sleep(K_MSEC(100));
 	}
-	++count;
-	if (rc == -EBADMSG) {
-		/* Sample overrun.  Ignore in polled mode. */
-		if (IS_ENABLED(CONFIG_LIS2DH_TRIGGER)) {
-			overrun = "[OVERRUN] ";
-		}
-		rc = 0;
-	}
-	if (rc == 0) {
-		rc = sensor_channel_get(sensor,
-					SENSOR_CHAN_ACCEL_XYZ,
-					accel);
-	}
-	if (rc < 0) {
-		printf("ERROR: Update failed: %d\n", rc);
-	} else {
-		printf("#%u @ %u ms: %sx %f , y %f , z %f",
-		       count, k_uptime_get_32(), overrun,
-		       sensor_value_to_double(&accel[0]),
-		       sensor_value_to_double(&accel[1]),
-		       sensor_value_to_double(&accel[2]));
-	}
-
-	if (IS_ENABLED(CONFIG_LIS2DH_MEASURE_TEMPERATURE)) {
-		if (rc == 0) {
-			rc = sensor_channel_get(sensor, SENSOR_CHAN_DIE_TEMP, &temperature);
-			if (rc < 0) {
-				printf("\nERROR: Unable to read temperature:%d\n", rc);
-			} else {
-				printf(", t %f\n", sensor_value_to_double(&temperature));
-			}
-		}
-
-	} else {
-		printf("\n");
-	}
-}
-
-#ifdef CONFIG_LIS2DH_TRIGGER
-static void trigger_handler(const struct device *dev,
-			    const struct sensor_trigger *trig)
-{
-	fetch_and_display(dev);
-}
-#endif
-
-void main(void)
-{
-	const struct device *sensor = DEVICE_DT_GET_ANY(st_lis2dh);
 
 	if (sensor == NULL) {
-		printf("No device found\n");
+		printk("No device found\n");
 		return;
 	}
 	if (!device_is_ready(sensor)) {
-		printf("Device %s is not ready\n", sensor->name);
+		printk("Device %s is not ready\n", sensor->name);
 		return;
 	}
 
-#if CONFIG_LIS2DH_TRIGGER
+#if CONFIG_LIS3DH_TRIGGER
 	{
 		struct sensor_trigger trig;
 		int rc;
@@ -93,7 +85,7 @@ void main(void)
 		trig.type = SENSOR_TRIG_DATA_READY;
 		trig.chan = SENSOR_CHAN_ACCEL_XYZ;
 
-		if (IS_ENABLED(CONFIG_LIS2DH_ODR_RUNTIME)) {
+		if (IS_ENABLED(CONFIG_LIS3DH_ODR_RUNTIME)) {
 			struct sensor_value odr = {
 				.val1 = 1,
 			};
@@ -102,28 +94,28 @@ void main(void)
 					     SENSOR_ATTR_SAMPLING_FREQUENCY,
 					     &odr);
 			if (rc != 0) {
-				printf("Failed to set odr: %d\n", rc);
+				printk("Failed to set odr: %d\n", rc);
 				return;
 			}
-			printf("Sampling at %u Hz\n", odr.val1);
+			printk("Sampling at %u Hz\n", odr.val1);
 		}
 
 		rc = sensor_trigger_set(sensor, &trig, trigger_handler);
 		if (rc != 0) {
-			printf("Failed to set trigger: %d\n", rc);
+			printk("Failed to set trigger: %d\n", rc);
 			return;
 		}
 
-		printf("Waiting for triggers\n");
+		printk("Waiting for triggers\n");
 		while (true) {
 			k_sleep(K_MSEC(2000));
 		}
 	}
-#else /* CONFIG_LIS2DH_TRIGGER */
-	printf("Polling at 0.5 Hz\n");
+#else /* CONFIG_LIS3DH_TRIGGER */
+	printk("Polling at 0.5 Hz\n");
 	while (true) {
 		fetch_and_display(sensor);
 		k_sleep(K_MSEC(2000));
 	}
-#endif /* CONFIG_LIS2DH_TRIGGER */
+#endif /* CONFIG_LIS3DH_TRIGGER */
 }
