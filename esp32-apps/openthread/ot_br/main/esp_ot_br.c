0ca46aa7693fd7cf048a2134fc7cd36053701bd9
LyubomirNachev <lubolubo04@gmail.com>
2023-02-06 21:07:26 +0200

    Working webpage change when a packet is received on the esp border router

--- esp32-apps/openthread/ot_br/main/esp_ot_br.c
+++ esp32-apps/openthread/ot_br/main/esp_ot_br.c
@@ -72,10 +72,8 @@
 #include <lwip/api.h>
 #include <lwip/netdb.h>
 
-#define LED_PIN 2 //
-char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
-char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
-int led_state = 0;
+char on_resp[] = "<!DOCTYPE html><html><head><style>body {background-color: rgb(219, 221, 123);}h1 {text-align:center;font-family:verdana;color: blue;} p {margin: auto;width: 320px; padding: 10px;border: 2px solid black;text-align:center;font-family:verdana;color: red;}</style><title>Page Title</title></head><body><h1>TEST</h1><p>Oh Oh!</p></body></html>";
+char off_resp[] = "<!DOCTYPE html><html><head><style>body {background-color: rgb(219, 221, 123);}h1 {text-align:center;font-family:verdana;color: blue;} p {margin: auto;width: 320px; padding: 10px;border: 2px solid black;text-align:center;font-family:verdana;color: red;}</style><title>Page Title</title></head><body><h1>TEST</h1><p>Hello Thread!</p></body></html>";
 
 #define TAG "esp_ot_br"
 
@@ -220,14 +218,16 @@ static void ot_task_worker(void *aContext)
     vTaskDelete(NULL);
 }
 
+char rx_buffer[128];
+int len;
+
 static void udp_socket_server_task(void *pvParameters)
 {
     char addr_str[128];
     char *payload = "This message is from server\n";
-    char rx_buffer[128];
     esp_err_t ret = ESP_OK;
     int err = 0;
-    int len;
+    
     int listen_sock;
 
     int port = CONFIG_OPENTHREAD_CLI_UDP_SERVER_PORT;
@@ -294,15 +294,16 @@ exit:
 
 
 
-
-
 esp_err_t send_web_page(httpd_req_t *req)
 {
     int response;
-    if (led_state == 0)
+    if (len == 12){
         response = httpd_resp_send(req, off_resp, HTTPD_RESP_USE_STRLEN);
-    else
+        len = 0;
+    }
+    else{
         response = httpd_resp_send(req, on_resp, HTTPD_RESP_USE_STRLEN);
+    }
     return response;
 }
 esp_err_t get_req_handler(httpd_req_t *req)
@@ -310,17 +311,13 @@ esp_err_t get_req_handler(httpd_req_t *req)
     return send_web_page(req);
 }
 
-esp_err_t led_on_handler(httpd_req_t *req)
+esp_err_t messg_rcv_handler(httpd_req_t *req)
 {
-    gpio_set_level(LED_PIN, 1);
-    led_state = 1;
     return send_web_page(req);
 }
 
-esp_err_t led_off_handler(httpd_req_t *req)
+esp_err_t messg_no_rcv_handler(httpd_req_t *req)
 {
-    gpio_set_level(LED_PIN, 0);
-    led_state = 0;
     return send_web_page(req);
 }
 
@@ -331,15 +328,15 @@ httpd_uri_t uri_get = {
     .user_ctx = NULL};
 
 httpd_uri_t uri_on = {
-    .uri = "/led2on",
+    .uri = "/Received",
     .method = HTTP_GET,
-    .handler = led_on_handler,
+    .handler = messg_rcv_handler,
     .user_ctx = NULL};
 
 httpd_uri_t uri_off = {
-    .uri = "/led2off",
+    .uri = "/Oh_oh",
     .method = HTTP_GET,
-    .handler = led_off_handler,
+    .handler =messg_no_rcv_handler,
     .user_ctx = NULL};
 
 httpd_handle_t setup_server(void)
@@ -419,12 +416,7 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
-    // GPIO initialization
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-
-    led_state = 0;
-    ESP_LOGI(TAG, "LED Control Web Server is running ... ...\n");
+    ESP_LOGI(TAG, "Web Server is running ... ...\n");
     setup_server();
 }
 
