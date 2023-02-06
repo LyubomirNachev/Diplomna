b1f8b262410bf085713971c210cdf91787a2bdd3
LyubomirNachev <lubolubo04@gmail.com>
2023-02-06 19:56:13 +0200

    Working http server example

--- esp32-apps/openthread/ot_br/main/esp_ot_br.c
+++ esp32-apps/openthread/ot_br/main/esp_ot_br.c
@@ -13,6 +13,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h> //
 #include <string.h>
 
 #include "esp_check.h"
@@ -57,16 +58,24 @@
 
 #include "esp_ot_udp_socket.h"
 
-#include "esp_check.h"
-#include "esp_err.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_openthread_lock.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "lwip/err.h"
 #include "lwip/mld6.h"
 #include "lwip/sockets.h"
+
+#include "esp_system.h"
+#include "spi_flash_mmap.h"
+#include <esp_http_server.h>
+#include "freertos/event_groups.h"
+#include "driver/gpio.h"
+#include <lwip/sockets.h>
+#include <lwip/sys.h>
+#include <lwip/api.h>
+#include <lwip/netdb.h>
+
+#define LED_PIN 2 //
+char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
+char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
+int led_state = 0;
 
 #define TAG "esp_ot_br"
 
@@ -274,6 +283,103 @@ exit:
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+esp_err_t send_web_page(httpd_req_t *req)
+{
+    int response;
+    if (led_state == 0)
+        response = httpd_resp_send(req, off_resp, HTTPD_RESP_USE_STRLEN);
+    else
+        response = httpd_resp_send(req, on_resp, HTTPD_RESP_USE_STRLEN);
+    return response;
+}
+esp_err_t get_req_handler(httpd_req_t *req)
+{
+    return send_web_page(req);
+}
+
+esp_err_t led_on_handler(httpd_req_t *req)
+{
+    gpio_set_level(LED_PIN, 1);
+    led_state = 1;
+    return send_web_page(req);
+}
+
+esp_err_t led_off_handler(httpd_req_t *req)
+{
+    gpio_set_level(LED_PIN, 0);
+    led_state = 0;
+    return send_web_page(req);
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = get_req_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_on = {
+    .uri = "/led2on",
+    .method = HTTP_GET,
+    .handler = led_on_handler,
+    .user_ctx = NULL};
+
+httpd_uri_t uri_off = {
+    .uri = "/led2off",
+    .method = HTTP_GET,
+    .handler = led_off_handler,
+    .user_ctx = NULL};
+
+httpd_handle_t setup_server(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(server, &uri_get);
+        httpd_register_uri_handler(server, &uri_on);
+        httpd_register_uri_handler(server, &uri_off);
+    }
+
+    return server;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void app_main(void)
 {
     // Used eventfds:
@@ -301,6 +407,25 @@ void app_main(void)
         ESP_ERROR_CHECK(mdns_hostname_set("esp-ot-br"));
         xTaskCreate(ot_task_worker, "ot_br_main", 20480, xTaskGetCurrentTaskHandle(), 5, NULL);
         xTaskCreate(udp_socket_server_task, "ot_udp_scoket_server", 4096, xTaskGetCurrentTaskHandle(), 4, NULL);
+
+
+
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // GPIO initialization
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    led_state = 0;
+    ESP_LOGI(TAG, "LED Control Web Server is running ... ...\n");
+    setup_server();
 }
 
 
