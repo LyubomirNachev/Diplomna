06f3be03354019f893428acd0081fb1626c38370
LyubomirNachev <lubolubo04@gmail.com>
2023-02-11 01:34:44 +0200

    Added display functionality to the BR

--- esp32-apps/openthread/ot_br/main/esp_ot_br.c
+++ esp32-apps/openthread/ot_br/main/esp_ot_br.c
@@ -79,6 +79,12 @@ char off_resp[] = "<!DOCTYPE html><html><head><style>body {background-color: rgb
 //
 
 
+//display
+#include "ssd1306.h"
+
+//
+
+
 
 
 
@@ -448,6 +454,14 @@ void app_main(void)
 
         ESP_LOGI(TAG, "Socket is running ... ...\n");
         start_webserver();
+
+        SSD1306_t dev;
+	    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);	
+        ssd1306_init(&dev, 128, 64);
+        ssd1306_clear_screen(&dev, false);
+        ssd1306_contrast(&dev, 0xff);
+        ssd1306_display_text(&dev, 1, "IP:          ", 16, false);
+        ssd1306_display_text(&dev, 2, "192.168.0.106", 16, false);
 }
 
 
