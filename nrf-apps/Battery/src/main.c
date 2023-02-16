545d18cda8c3060c0ba646995f8096de9b1a0020
LyubomirNachev <lubolubo04@gmail.com>
2023-02-16 02:02:55 +0200

    Working adc pin read

--- nrf-apps/Battery/src/main.c
+++ nrf-apps/Battery/src/main.c
@@ -1,6 +1,13 @@
-#include "AnalogIn.h"
-#include <drivers/adc.h>
-#include <string.h>
+#include <zephyr/zephyr.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/drivers/adc.h>
+
 
 // Simple analog input method
 // this just reads a sample then waits then returns it
@@ -8,136 +15,67 @@
 // ADC Sampling Settings
 // doc says that impedance of 800K == 40usec sample time
 
-#define ADC_DEVICE_NAME		DT_LABEL(DT_ALIAS(adcctrl))
+#include <drivers/adc.h>
+#include <string.h>
+
+#define ADC_NODE DT_NODELABEL(adc)
+static const struct device *adc_dev = DEVICE_DT_GET(ADC_NODE);
+
 #define ADC_RESOLUTION		10
-#define ADC_GAIN			ADC_GAIN_1_6
+#define ADC_GAIN			ADC_GAIN_1_5
 #define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME(ADC_ACQ_TIME_MICROSECONDS, 40)
-#define BUFFER_SIZE			6
+#define ADC_PORT SAADC_CH_PSELP_PSELP_AnalogInput0
+#define ADC_CHANNEL 0
 
-static bool _IsInitialized = false;
-static uint8_t _LastChannel = 250;
-static int16_t m_sample_buffer[BUFFER_SIZE];
-
-// the channel configuration with channel not yet filled in
-static struct adc_channel_cfg m_1st_channel_cfg = {
-	.gain             = ADC_GAIN,
-	.reference        = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id       = 0, // gets set during init
-	.differential	  = 0,
-#if CONFIG_ADC_CONFIGURABLE_INPUTS
-	.input_positive   = 0, // gets set during init
+struct adc_channel_cfg chl0_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQ_TIME_DEFAULT,
+	.channel_id = ADC_CHANNEL,
+#ifdef CONFIG_ADC_NRFX_SAADC
+	.input_positive = ADC_PORT
 #endif
+};  
+
+int16_t sample_buffer[1];
+struct adc_sequence sequence = {
+	.channels = BIT(ADC_CHANNEL),
+	.buffer = sample_buffer,
+	.buffer_size = sizeof(sample_buffer),
+	.resolution = ADC_RESOLUTION
 };
 
-// return device* for the adc
-static const struct device* getAdcDevice(void)
-{
-	return device_get_binding(ADC_DEVICE_NAME);
+void main(void){
+
+	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+	int err;
+	if (!device_is_ready(adc_dev)){
+		printk("adc_dev not ready\n");
+		return;
+	}
+	err=adc_channel_setup(adc_dev, &chl0_cfg);
+	while(1){
+		err = adc_read(adc_dev, &sequence);
+		printk("ADC: %d\n", sample_buffer[0]);
+		int32_t mv_value = sample_buffer[0];
+		int32_t adc_vref = adc_ref_internal(adc_dev);
+		adc_raw_to_millivolts(adc_vref, ADC_GAIN, ADC_RESOLUTION, &mv_value);
+		printk("ADC converted: %d mV\n", mv_value);
+		k_msleep(1000);
+	}
+
+
+
 }
-
-// initialize the adc channel
-static const struct device* init_adc(int channel)
-{
-	int ret;
-	const struct device *adc_dev = getAdcDevice();
-	if(_LastChannel != channel)
-	{
-		_IsInitialized = false;
-		_LastChannel = channel;
-	}
-
-	if ( adc_dev != NULL && !_IsInitialized)
-	{
-		// strangely channel_id gets the channel id and input_positive gets id+1
-		m_1st_channel_cfg.channel_id = channel;
-#if CONFIG_ADC_CONFIGURABLE_INPUTS
-        m_1st_channel_cfg.input_positive = channel+1,
-#endif
-		ret = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
-		if(ret != 0)
-		{
-			//LOG_INF("Setting up of the first channel failed with code %d", ret);
-			adc_dev = NULL;
-		}
-		else
-		{
-			_IsInitialized = true;	// we don't have any other analog users
-		}
-	}
-	
-	memset(m_sample_buffer, 0, sizeof(m_sample_buffer));
-	return adc_dev;
-}
-
-// ------------------------------------------------
-// read one channel of adc
-// ------------------------------------------------
-static int16_t readOneChannel(int channel)
-{
-	const struct adc_sequence sequence = {
-		.options     = NULL,				// extra samples and callback
-		.channels    = BIT(channel),		// bit mask of channels to read
-		.buffer      = m_sample_buffer,		// where to put samples read
-		.buffer_size = sizeof(m_sample_buffer),
-		.resolution  = ADC_RESOLUTION,		// desired resolution
-		.oversampling = 0,					// don't oversample
-		.calibrate = 0						// don't calibrate
-	};
-
-	int ret;
-	int16_t sample_value = BAD_ANALOG_READ;
-	const struct device *adc_dev = init_adc(channel);
-	if (adc_dev)
-	{
-		ret = adc_read(adc_dev, &sequence);
-		if(ret == 0)
-		{
-			sample_value = m_sample_buffer[0];
-		}
-	}
-
-	return sample_value;
-}
-
-// ------------------------------------------------
-// high level read adc channel and convert to float voltage
-// ------------------------------------------------
-float AnalogRead(int channel)
-{
-
-	int16_t sv = readOneChannel(channel);
-	if(sv == BAD_ANALOG_READ)
-	{
-		return sv;
-	}
-
-	// Convert the result to voltage
-	// Result = [V(p) - V(n)] * GAIN/REFERENCE / 2^(RESOLUTION)
-																				  
-	int multip = 256;
-	// find 2**adc_resolution
-	switch(ADC_RESOLUTION)
-				
-	{
-		default :
-		case 8 :
-			multip = 256;
-			break;
-		case 10 :
-			multip = 1024;
-			break;
-		case 12 :
-			multip = 4096;
-			break;
-		case 14 :
-			multip = 16384;
-			break;
-	}
-	
-	// the 3.6 relates to the voltage divider being used in my circuit
-	float fout = (sv * 3.6 / multip);
-	return fout;
-}
-
